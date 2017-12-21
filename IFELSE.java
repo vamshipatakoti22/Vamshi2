0d8c8334df61a5e3c49fffc2fb66952965540f86
vamshipatakoti22 <34724617+vamshipatakoti22@users.noreply.github.com>
2017-12-20 17:45:06 -0800

    IF ELSE Statement
    IF ELSE Statement was added to the GIT Repository

---
+++ IFELSE.java
@@ -0,0 +1,21 @@
+package Day1.example;
+
+public class ExampleIf {
+
+	public static void main(String[] args) {
+		
+		int x=30;
+		int y=0;
+		
+		if(x<y) {			
+			System.out.println("x is less than y");
+		}
+		else if (x==y) {
+			System.out.println("x is equal to y");
+			
+		}
+		else {
+			System.out.println("x is greater than y");
+		}
+	}
+}
