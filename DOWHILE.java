d56d33680499af70f79fbe17279690940d42a5a9
vamshipatakoti22 <34724617+vamshipatakoti22@users.noreply.github.com>
2017-12-20 17:50:13 -0800

    DO WHILE Statement
    DO WHILE Statement Doc has been added to the repository.

---
+++ DOWHILE.java
@@ -0,0 +1,15 @@
+package Day1.example;
+
+public class ExampleDoWhile {
+
+	public static void main(String[] args)
+	
+	{
+		int x = 10;
+		do {
+			System.out.println("x = " + x);
+			x--;
+		}while(x>0);
+	}
+	
+}
